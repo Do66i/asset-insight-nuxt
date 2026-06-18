216e2ea279a35673c2ce587b085c20aaf290519b
Do66i <xoxo.madlife@gmail.com>
2026-06-18 12:20:54 +0900

    chore: ESLint 설정 및 웹스톰 자동 저장 포맷터 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2026-06-18',
+    modules: [
+        '@nuxt/eslint'
+    ],
   devtools: { enabled: true }
 })
